43d7f971abc1471c32b6e23d43f8cd03f9fcacff
sjohnston88 <samjohnston@notonthehighstreet.com>
2018-04-08 22:09:21 +0100

    npm init, git ignore

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const filter = `?filter[playerNames]=${players}`
 
 const headers = {
   'accept': 'application/vnd.api+json',
-  'Authorization': 'Bearer <API-KEY></API-KEY>'
+  'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiI1YTdlYTc1MC0xYjAyLTAxMzYtNTY0YS0wMmQ4YTIwZTk4MjQiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTIyOTM0NTY2LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6InNhbWpvaG5zdG9ucHViZyIsInNjb3BlIjoiY29tbXVuaXR5IiwibGltaXQiOjEwfQ.5VxcMpZY4MNALdP_7RDJxz7DXZ0V1qNIYaWj7kzAGvg'
 };
 
 const getPlayerData = () => {
